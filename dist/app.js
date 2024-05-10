712f21dea657bb90ab125f8aacaa88e5f29d9af9
Nathan Sodja <sodjanathan@gmail.com>
2024-05-10 19:57:40 +0000

    setup .env

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,19 @@
 import * as fs from "fs";
 import * as path from "path";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import "dotenv/config";
 async function readFile(relativeFilePath) {
     const currentFilePath = new URL(import.meta.url).pathname;
     const currentDirPath = path.dirname(currentFilePath);
-    console.log("currentFilePath" + currentFilePath);
-    console.log("currentDirPath" + currentFilePath);
     const filePath = path.resolve(currentDirPath, relativeFilePath);
+    console.log(process.env.SUPABASE_API_KEY);
     try {
-        const data = await fs.promises.readFile(filePath, "utf8");
-        console.log(data);
+        const text = await fs.promises.readFile(filePath, "utf8");
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 500,
+            chunkOverlap: 50, // 10% of chunk size is good rule of thumb
+        });
+        const output = await splitter.createDocuments([text]);
     }
     catch (error) {
         console.error(`Error reading file ${filePath}: ${error.message}`);
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import * as fs from "fs";
 import * as path from "path";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import "dotenv/config";
 
 async function readFile(relativeFilePath: string): Promise<void> {
-
   const currentFilePath = new URL(import.meta.url).pathname;
   const currentDirPath = path.dirname(currentFilePath);
+  const filePath = path.resolve(currentDirPath, relativeFilePath);
+  console.log(process.env.SUPABASE_API_KEY);
 
-  console.log("currentFilePath" + currentFilePath);
-  console.log("currentDirPath" + currentFilePath);
-
-	const filePath = path.resolve(currentDirPath, relativeFilePath);
-	
   try {
-    const data = await fs.promises.readFile(filePath, "utf8");
-    console.log(data);
+    const text = await fs.promises.readFile(filePath, "utf8");
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      chunkOverlap: 50, // 10% of chunk size is good rule of thumb
+    });
+
+    const output = await splitter.createDocuments([text]);
   } catch (error) {
     console.error(
       `Error reading file ${filePath}: ${(error as Error).message}`,
