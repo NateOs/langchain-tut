9440d7cd6e0496171481d00d1344d22a2649fab8
Nathan Sodja <sodjanathan@gmail.com>
2024-05-16 16:32:47 +0000

    wip: insertion failed

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,14 @@
 import * as fs from "fs";
 import * as path from "path";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import "dotenv/config";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createClient } from "@supabase/supabase-js";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 async function readFile(relativeFilePath) {
     const currentFilePath = new URL(import.meta.url).pathname;
     const currentDirPath = path.dirname(currentFilePath);
     const filePath = path.resolve(currentDirPath, relativeFilePath);
-    console.log(process.env.SUPABASE_API_KEY);
     try {
         const text = await fs.promises.readFile(filePath, "utf8");
         const splitter = new RecursiveCharacterTextSplitter({
@@ -14,9 +16,26 @@ async function readFile(relativeFilePath) {
             chunkOverlap: 50, // 10% of chunk size is good rule of thumb
         });
         const output = await splitter.createDocuments([text]);
+        console.log(output);
+        const supabaseProjectUrl = process.env.SUPABASE_URL_LC_CHATBOT;
+        const supabaseApiKey = process.env.SUPABASE_API_KEY;
+        const openAIKey = process.env.OPENAI_API_KEY;
+        let client;
+        // create embeddings
+        const embeddings = new OpenAIEmbeddings({
+            apiKey: openAIKey,
+            batchSize: 512,
+            model: "text-embedding-3-large",
+        });
+        // save embeddings to database
+        await SupabaseVectorStore.fromDocuments(output, embeddings, {
+            client: client ??
+                createClient(supabaseProjectUrl, supabaseApiKey),
+            tableName: "documents",
+        });
     }
     catch (error) {
-        console.error(`Error reading file ${filePath}: ${error.message}`);
+        console.error(error);
     }
 }
 const relativeFilePath = "../scrimba-info.txt";
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import * as fs from "fs";
 import * as path from "path";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import "dotenv/config";
+
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createClient } from "@supabase/supabase-js";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 
 async function readFile(relativeFilePath: string): Promise<void> {
   const currentFilePath = new URL(import.meta.url).pathname;
   const currentDirPath = path.dirname(currentFilePath);
   const filePath = path.resolve(currentDirPath, relativeFilePath);
-  console.log(process.env.SUPABASE_API_KEY);
 
   try {
     const text = await fs.promises.readFile(filePath, "utf8");
@@ -17,10 +20,28 @@ async function readFile(relativeFilePath: string): Promise<void> {
     });
 
     const output = await splitter.createDocuments([text]);
+
+    const supabaseProjectUrl = process.env.SUPABASE_URL_LC_CHATBOT;
+    const supabaseApiKey = process.env.SUPABASE_API_KEY;
+    const openAIKey = process.env.OPENAI_API_KEY;
+    let client;
+
+    // create embeddings
+    const embeddings = new OpenAIEmbeddings({
+      apiKey: openAIKey,
+      batchSize: 512,
+      model: "text-embedding-3-large",
+    });
+
+    // save embeddings to database
+    await SupabaseVectorStore.fromDocuments(output, embeddings, {
+      client:
+        client ??
+        createClient(supabaseProjectUrl as string, supabaseApiKey as string),
+      tableName: "documents",
+    });
   } catch (error) {
-    console.error(
-      `Error reading file ${filePath}: ${(error as Error).message}`,
-    );
+    console.error(error);
   }
 }
 
